7c5981aa74727d9d92904230f64252e9ac278fa3
Karol Radziuk <kr@ambsoft.pl>
2015-05-11 10:56:04 +0200

    Localization: pol-PL translation added

---
+++ translations/pol-PL/translation.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>extension/ezstarrating/datatype</name>
+    <message>
+        <source>Star Rating</source>
+        <comment>Datatype name</comment>
+        <translation>Star Rating</translation>
+    </message>
+    <message>
+        <source>Currently %current_rating out of 5 Stars.</source>
+        <translation>Obecnie %current_rating na 5 gwiazdek.</translation>
+    </message>
+    <message>
+        <source>Rate %rating stars out of 5</source>
+        <translation>Przyznaj %rating gwiazdki na 5 możliwych</translation>
+    </message>
+    <message>
+        <source>Rating: %current_rating/5</source>
+        <translation>Ocena: %current_rating/5</translation>
+    </message>
+    <message>
+        <source>%rating_count votes cast</source>
+        <translation>%rating_count oddanych głosów</translation>
+    </message>
+    <message>
+        <source>Thank you for rating!</source>
+        <comment>When rating</comment>
+        <translation>Dziękujemy za ocenę!</translation>
+    </message>
+    <message>
+        <source>You have already rated this page, you can only rate it once!</source>
+        <comment>When rating</comment>
+        <translation>Ta strona została już przez Ciebie oceniona, ocenić możesz tylko raz!</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end or %create_link_startcreate a user account%create_link_end to rate this page.</source>
+        <translation>%login_link_startZaloguj się%login_link_end lub %create_link_startzałóż konto%create_link_end aby ocenić tę stronę.</translation>
+    </message>
+    <message>
+        <source>%login_link_startLog in%login_link_end to rate this page.</source>
+        <translation>%login_link_startZaloguj się%login_link_end aby ocenić tę stronę.</translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <translation>Wyłączone</translation>
+    </message>
+    <message>
+        <source>disabled</source>
+        <translation>wyłączone</translation>
+    </message>
+    <message>
+        <source>Your rating has been changed, thanks for rating!</source>
+        <comment>When rating</comment>
+        <translation>Twoja ocena została zmieniona, dzięki za ocenę!</translation>
+    </message>
+    <message>
+        <source>You don&apos;t have access to rate this page.</source>
+        <translation>Nie masz uprawnień do oceniania tej strony.</translation>
+    </message>
+</context>
+<context>
+    <name>kernel/content</name>
+    <message>
+        <source>Top Level Nodes</source>
+        <translation>Węzły najwyższego poziomu</translation>
+    </message>
+</context>
+</TS>
